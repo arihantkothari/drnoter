fe8841fec524017f4638eff4185486554b03642a
arihantkothari <arihantkothari17@gmail.com>
2020-06-03 02:33:14 +0530

    Google Sign In works perfectly. Looks ugly.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+// 1. Import the libs you need
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { MemberComponent } from './member/member.component';
 
+// 2. Add your credentials from step 1
+const config = {
+  apiKey: 'AIzaSyCUDUZ3F_HwOPF74FM6gc1O2f_N4c5t5EA',
+  authDomain: 'noter-ecac1.firebaseapp.com',
+  databaseURL: 'https://noter-ecac1.firebaseio.com',
+  projectId: 'noter-ecac1',
+  storageBucket: 'noter-ecac1.appspot.com',
+  messagingSenderId: '532226072842'
+};
+const appRoutes: Routes = [
+  { path: 'loginPage', component: AppComponent },
+];
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    // 3. Initialize
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule, // firestore
+    AngularFireAuthModule, // auth
+    AngularFireStorageModule // storage
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [ AppComponent, MemberComponent ],
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+
+
+export class AppModule {}
